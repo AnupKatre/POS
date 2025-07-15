737cb020d245981cf93c6ef3d326df425adfb529
Anup Katre <anupkatre2@gmail.com>
2025-07-15 11:30:26 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/AppBody.tsx
+++ src/components/AppBody.tsx
@@ -1,21 +1,19 @@
 
 'use client';
 
-import { useSidebar } from '@/hooks/use-sidebar';
 import AppSidebar from '@/components/AppSidebar';
 import { Toaster } from './ui/toaster';
 
 export default function AppBody({ children }: { children: React.ReactNode }) {
-    const { isExpanded } = useSidebar();
     return (
-      <body className="font-body antialiased" data-sidebar-is-expanded={isExpanded}>
+      <div className="font-body antialiased">
         <div className="flex flex-col h-screen w-full bg-secondary">
-          <main className="flex-1 flex flex-col overflow-y-auto relative z-10 rounded-2xl bg-background shadow-lg m-4 transition-[margin] duration-300 ease-in-out group-data-[sidebar-is-expanded=true]:mb-24 group-data-[sidebar-is-expanded=false]:mb-4">
+          <main className="flex-1 flex flex-col overflow-y-auto relative z-10 rounded-2xl bg-background shadow-lg m-4 mb-24">
             {children}
           </main>
           <AppSidebar />
         </div>
         <Toaster />
-      </body>
+      </div>
     );
   }
